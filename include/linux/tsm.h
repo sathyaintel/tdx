ee09fbabe2eac73341ab38dd7c187d0e94cca446
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2023-11-07 03:31:03 +0000

    config-tsm: Add interface to configure remote server GUID
    For scalability or isolation requirements, CSPs may choose to host VMM
    features as a service in a remote VM. For example, in TDX, service VM
    concept is adapted for features like attestation, vTPM, etc.
    
    To handle the use case where report generation service hosted in a
    remote VM, add a new interface to allow user to pass the GUID of the
    remote report generation service which will be used by the VMM to
    identify the remote server and forward the request/response.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- include/linux/tsm.h
+++ include/linux/tsm.h
@@ -20,11 +20,13 @@
  * @privlevel: optional privilege level to associate with @outblob
  * @inblob_len: sizeof @inblob
  * @inblob: arbitrary input data
+ * @remote_guid: GUID of the report generation server
  */
 struct tsm_desc {
 	unsigned int privlevel;
 	size_t inblob_len;
 	u8 inblob[TSM_INBLOB_MAX];
+	guid_t remote_guid;
 };
 
 /**
